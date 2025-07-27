0a04a2b3760e51229e83c2ffb807646cd2a47bd3
stian <stian@mandarindesign.no>
2025-07-27 21:55:39 +0200

    feat: create reload script

--- apps/waybar/run.sh
+++ apps/waybar/run.sh
@@ -2,6 +2,7 @@
 
 # Kill any existing waybar processes
 pkill waybar
+sleep 1
 
 # Start waybar
-waybar -c ~/.config/hypr/apps/waybar/config.jsonc -s ~/.config/hypr/apps/waybar/$(~/.config/hypr/scripts/theme/get-theme.sh).css &
+waybar -c ~/.config/hypr/apps/waybar/config.jsonc -s ~/.config/hypr/apps/waybar/$(~/.config/hypr/scripts/theme/get-theme.sh).css
